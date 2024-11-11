c7b25addf5bce8a52918c175df8850d5e447c806
Oleh Didushok <didushokov@gmail.com>
2024-11-11 13:39:49 +0200

    Initial setup

---
+++ AvocadoApp/AvocadoApp/AvocadoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoAppApp.swift
+//  AvocadoApp
+//
+//  Created by Oleh on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
